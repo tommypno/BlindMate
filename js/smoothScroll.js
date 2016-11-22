c162ee3631337d1bb5b3f6529e6b8bc44f1ad512
Tommy Pedersen <post@tommyp.no>
2016-11-22 17:10:25 +0100

    dagens

--- js/smoothScroll.js
+++ js/smoothScroll.js
@@ -27,7 +27,7 @@ function elmYPosition(eID) {
 
 // So how does this function work, exactly? Well, say the destination element is half way down the page. What this function does is loop through the offsetParents, adding the offsetTop values to the y variable until it arrives at the top of the page. This will compute the elements true Y position.
 
-//smoothScroll( string elementID )
+// smoothScroll( string elementID )
 
 // Now that we're able to obtain the start and stop Y coordinates, we're ready to perform the actual scroll operation. Here's the function, I'll explain it below:
 
@@ -54,3 +54,15 @@ function smoothScroll(eID) {
         leapY -= step; if (leapY < stopY) leapY = stopY; timer++;
     }
 }
+
+// The first thing this function does is fetch the start and stop Y coordinates via the functions covered above. Next, it compares these two values to determine the distance between them. When setting the distance variable, an alternate control structure is used to determine which point should be subtracted from the other (depends on whether it needs to scroll up or down).
+
+// Once the start, stop and distance values are set, a check is done to see whether the destination element is less than 100 pixels away, and if so it simply jumps to it. If the destination point is further away the function continues by dynamically setting the scroll speed (distance divided by 100) without letting it exceed 20, step size (distance to jump each time the visible top of page Y coordinate is changed, thus moving closer to the destination), the leapY (next coordinate to jump to) and the initial timer value of 0.
+
+// At this point, a check is performed to determine whether the destination point is greater than the starting point (scrolling down). If so, the if is entered and the scroll is performed otherwise the for loop at the very bottom is hit, which performs an upward scroll.
+
+// Here is how the first for loop works (scroll up). The loop starts out with i = the start Y coordinate and loops until it hits the stop Y coordinate, incrementing i the step size per iteration. Inside the loop there is a setTimeout which runs window.scrollTo with the next Y coordinate as its value (leapY). The actual timeout value is determined by multiplying the timer value by the speed. So, the timeout value for the first iteration would be 0 * speed, the second iteration would use 1 * speed and so on.
+
+// After the setTimeout, the leapY value is incremented the step size (the next Y coordinate to jump to). A check is also performed to ensure the leapY value doesn't exceed the destination coordinate.
+
+// The scroll up for loop works the exact opposite of the scroll down loop we just covered. Hopefully I've done a decent job of explaining how this function works.
